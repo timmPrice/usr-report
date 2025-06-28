0e010defd9664922ec00530690821cd90aa8ba27
timmPrice <timmPrice6@gmail.com>
2025-06-28 12:17:56 -0400

    starting maven package

---
+++ src/main/java/com/price/usr-report/report.java
@@ -0,0 +1,13 @@
+package com.price.usrreport;
+
+/**
+ * Hello world!
+ *
+ */
+public class report 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
