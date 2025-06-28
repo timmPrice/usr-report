46dc8d99b22e23589db4cf202bc1ad07456ca8c6
timmPrice <timmPrice6@gmail.com>
2025-06-28 16:30:52 -0400

    reporting list of active players to node server

--- src/main/java/com/price/usr-report/Report.java
+++ src/main/java/com/price/usr-report/Report.java
@@ -1,5 +1,10 @@
 package com.price.usrreport;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
 import java.util.HashSet;
 import java.util.Set;
 import org.bukkit.entity.Player;
@@ -31,7 +36,7 @@ public class Report extends JavaPlugin implements Listener {
         onlinePlayers.add(player.getName());
 
         getLogger().info("Player joined: " + player.getName());
-        getLogger().info("Online players (" + onlinePlayers.size() + "): " + String.join(", ", onlinePlayers));
+        sendPlayerListToWebsite(onlinePlayers);
     }
 
     @EventHandler
@@ -40,6 +45,30 @@ public class Report extends JavaPlugin implements Listener {
         onlinePlayers.remove(player.getName());
 
         getLogger().info("Player left: " + player.getName());
-        getLogger().info("Online players (" + onlinePlayers.size() + "): " + String.join(", ", onlinePlayers));
+        sendPlayerListToWebsite(onlinePlayers);
+    }
+    
+    public void sendPlayerListToWebsite(Set<String> onlinePlayers) {
+        try {
+            URL url = new URL("http://localhost:3000/api/active"); 
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            String json = "{\"players\": [\"" + String.join("\",\"", onlinePlayers) + "\"]}";
+
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = json.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+                getLogger().info("sending json " + json);
+            }
+
+            int code = connection.getResponseCode();
+            getLogger().info("POST response code: " + code);
+            
+        } catch (Exception e) {
+                getLogger().severe("Failed to send player list to website: " + e.getMessage());
+        }
     }
 }
